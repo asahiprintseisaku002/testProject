6832a686c5e0066bcacc2001fde25fe94039d92c
github <166389720+asahiprintseisaku002@users.noreply.github.com.>
2025-08-07 19:27:14 +0900

    js

--- lejade-shumokucho-main/js/topAnimation.js
+++ lejade-shumokucho-main/js/topAnimation.js
@@ -75,7 +75,7 @@ window.addEventListener('load', () => {
                                             });
                                         }
                                     });
-                                }, 600); // 600ms後に.phase5Imageを表示
+                                }, 1000); // 600ms後に.phase5Imageを表示
                             }
                         });
 
