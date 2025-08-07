443ecd60dddcc92cc5f8d31a336fa4dded6844a7
github <166389720+asahiprintseisaku002@users.noreply.github.com.>
2025-08-07 20:08:58 +0900

    le jade erase

--- lejade-shumokucho-main/js/height-catch.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function ($) {
-    let vh = $("header").height();
-    $("html").css("--header-vh", vh + "px");
-});
-
-jQuery(document).ready(function ($) {
-    let vh = $("header").outerHeight();
-    $("html").css("--outheader-vh", vh + "px");
-});
